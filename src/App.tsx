8f0b0b04134c382993f823fcd37c3d5a5fb53d6c
JMakoti <josephmwamuye11@gmail.com>
2025-12-20 19:02:38 +0300

    chore:project initialization

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import "./components/viewarticle";
+import ViewArticle from "./components/viewarticle";
+
+function App() {
+  return (
+    <>
+      <div>
+        <ViewArticle />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/viewarticle.tsx
@@ -0,0 +1,12 @@
+import AddArticle from "./addarticle";
+
+export default function ViewArticle() {
+  return (
+    <div>
+      <h1>View Articles</h1>
+      <p>
+        <AddArticle />
+      </p>
+    </div>
+  );
+}
