26728c1be2db58edd28259ce870fb46b71e4c030
JMakoti <josephmwamuye11@gmail.com>
2025-12-28 12:44:40 +0300

    create the article editor

--- src/components/createarticle.tsx
+++ src/components/createarticle.tsx
@@ -1,13 +1,42 @@
-import bgCover from "../assets/img/bg-cover.png";
+import { useEffect, useRef } from "react";
+import bgCover from "@/assets/img/bg-cover.png";
+import EditorJS from "@editorjs/editorjs";
+import { tools } from "./articleEditor";
 
 export default function CreateArticle() {
+  const editorRef = useRef<EditorJS | null>(null);
+
+  useEffect(() => {
+    if (editorRef.current) return;
+    let editor = new EditorJS({
+      holder: "editorjs",
+      autofocus: true,
+      tools: tools,
+      // data:'',
+      placeholder: "Start writing your article...",
+      onReady: () => {
+        editorRef.current = editor;
+      },
+    });
+
+    return () => {
+      if (editorRef.current) {
+        editorRef.current.destroy();
+        editorRef.current = null;
+      }
+    };
+  }, []);
+
   return (
     <div
       className="bg-contain bg-center h-screen bg-no-repeat"
       style={{ backgroundImage: `url(${bgCover})` }}
     >
-      <div className="text-center items-center text-2xl font-bold pt-20">
+      {/* <div className="text-center items-center text-2xl font-bold pt-20">
         <h1>Write Articles</h1>
+      </div> */}
+      <div className="max-w-3xl mx-auto pt-20 bg-white/90 p-4 rounded">
+        <div id="editorjs"></div>
       </div>
     </div>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,23 +2,32 @@ import { FiEdit } from "react-icons/fi";
 import logo from "../assets/img/logo.png";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { Separator } from "./ui/separator";
 export default function NavBar() {
+  // Object Destructuring
+  const { pathname } = useLocation();
   return (
-    <div>
-      <div className="flex  flex-row items-center justify-between mx-auto p-2 max-w-7xl">
+    <div className="sticky top-0 z-40 w-full bg-white/80 backdrop-blur-sm border-b shadow-sm">
+      <div className="flex flex-row items-center justify-between mx-auto p-2 max-w-7xl">
         <div>
           <Link to="/">
             <img src={logo} alt="Blue Bird" width={60} height={60} />
           </Link>
         </div>
         <div className="flex flex-row items-center gap-4">
-          <Link to="/write">
-            <Button className="text-white py-2 px-4">
-              <FiEdit /> Write
-            </Button>
-          </Link>
+          {!pathname.includes("/write") ? (
+            <Link to="/write">
+              <Button className="text-white py-2 px-4">
+                <FiEdit /> Write
+              </Button>
+            </Link>
+          ) : (
+              <Button className="text-white py-2 px-4 bg-blue-600 hover:bg-blue-700">
+                Publish
+              </Button>
+          )}
+
           <Avatar>
             <AvatarImage
               src="https://images.pexels.com/photos/45853/grey-crowned-crane-bird-crane-animal-45853.jpeg"
