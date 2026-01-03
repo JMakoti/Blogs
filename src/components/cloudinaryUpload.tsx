a9903d58fe9b06b6889fb00df2160571653b23e3
JMakoti <josephmwamuye11@gmail.com>
2026-01-03 13:59:59 +0300

    fix:adjust uplaod

--- src/components/cloudinaryUpload.tsx
+++ src/components/cloudinaryUpload.tsx
@@ -9,7 +9,7 @@ export async function uploadToCloudinary(file: File){
   const response = await fetch(
     `https://api.cloudinary.com/v1_1/${
       import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
-    }/image/upload`,
+    }/auto/upload`,
     {
       method: "POST",
       body: formData,
